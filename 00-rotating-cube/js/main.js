5e7184eafd6741870794720e3a5eee6c14234301
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2013-04-07 16:20:12 +0200

    Add rotating cube experiment

---
+++ 00-rotating-cube/js/main.js
@@ -0,0 +1,47 @@
+var scene, camera, renderer;
+
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var SPEED = 0.01;
+
+function init() {
+    scene = new THREE.Scene();
+
+    initCube();
+    initCamera();
+    initRenderer();
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
+}
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initCube() {
+    cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2), new THREE.MeshNormalMaterial());
+    scene.add(cube);
+}
+
+function rotateCube() {
+    cube.rotation.x -= SPEED * 2;
+    cube.rotation.y -= SPEED;
+    cube.rotation.z -= SPEED * 3;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateCube();
+    renderer.render(scene, camera);
+}
+
+init();
+render();
