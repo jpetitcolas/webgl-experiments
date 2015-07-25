16851f24aa31a4f5a3478b8f8b1f392000a9baf0
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-07-25 20:28:23 +0200

    Add rotating marmelab logo

---
+++ 01-rotating-mesh/js/main.js
@@ -0,0 +1,64 @@
+var scene, camera, renderer;
+
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var SPEED = 0.01;
+
+function init() {
+    scene = new THREE.Scene();
+
+    initMesh();
+    initCamera();
+    initLights();
+    initRenderer();
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
+}
+
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initLights() {
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+
+var mesh = null;
+function initMesh() {
+    var loader = new THREE.JSONLoader();
+    loader.load('./marmelab.json', function(geometry, materials) {
+        mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
+        mesh.translation = THREE.GeometryUtils.center(geometry);
+        scene.add(mesh);
+    });
+}
+
+function rotateMesh() {
+    if (!mesh) {
+        return;
+    }
+
+    mesh.rotation.x -= SPEED * 2;
+    mesh.rotation.y -= SPEED;
+    mesh.rotation.z -= SPEED * 3;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateMesh();
+    renderer.render(scene, camera);
+}
+
+init();
+render();
